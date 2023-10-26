81d386c8d9e223dc2f85a4030f3c853f1357f42e
kris299b <kripag01@easv365.dk>
2023-10-26 12:54:47 +0200

    Todo-list visible

---
+++ scr/main.ts
@@ -0,0 +1,7 @@
+// let age: number = 30; // Explicitly declaring 'age' as a number
+
+// interface MyInterface {
+//     property1: dataType1;
+//     property2: dataType2;
+//     // ... more properties ... Here's the basic syntax for defining an interface:
+//   } 
